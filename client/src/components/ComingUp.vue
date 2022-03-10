75f9e7ed1d704dab5d1bc0b4399da5c4a0f2db30
Dominic <dgrondin2@gmail.com>
2022-03-09 20:29:00 -0800

    added template for ComingUp component

---
+++ client/src/components/ComingUp.vue
@@ -0,0 +1,34 @@
+<script setup>
+  defineProps({
+    songs: Array
+  });
+  function like(song) {
+    song.likes++;
+  }
+  function dislike(song) {
+    song.dislikes++;
+  }
+</script>
+
+<template>
+  <div>
+    <h2>Coming Up</h2>
+    <ol>
+      <li v-for="song in this.songs" :key="song.id">
+        <div class="song-details">
+          <span class="song-details_artist">{{ song.artist }}</span>
+          <span class="song-details_title">{{ song.song }}</span>
+        </div>
+        <div class="likes">
+          <button class="likes_add-dislike" @click="dislike(song)">-</button>
+          <span class="likes_total">{{ song.likes - song.dislikes }}</span>
+          <button class="likes_add-like" @click="like(song)">+</button>
+        </div>
+      </li>
+    </ol>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
