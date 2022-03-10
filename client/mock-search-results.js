ee2a4a7eacae0ba809ac15c9a997c54eb7615c1e
Dominic <dgrondin2@gmail.com>
2022-03-09 21:36:34 -0800

    add mock data (for now)

---
+++ client/mock-search-results.js
@@ -0,0 +1,46 @@
+export default {
+  "response": [
+      {
+          "artist_id": 38,
+          "artist": "Michael Jackson",
+          "artwork_small": "https://d2x3f3hu3pbot6.cloudfront.net/upload/live/artists/150/4/177984.jpg",
+          "artwork_large": "https://d2x3f3hu3pbot6.cloudfront.net/upload/live/artists/500/4/177984.jpg"
+      },
+      {
+          "artist_id": 135,
+          "artist": "Queen",
+          "artwork_small": "https://d2x3f3hu3pbot6.cloudfront.net/upload/live/artists/150/4/514.jpg",
+          "artwork_large": "https://d2x3f3hu3pbot6.cloudfront.net/upload/live/artists/500/4/514.jpg"
+      },
+      {
+          "artist_id": 95,
+          "artist": "Guns N' Roses",
+          "artwork_small": "https://d2x3f3hu3pbot6.cloudfront.net/upload/live/artists/150/3/363.jpg",
+          "artwork_large": "https://d2x3f3hu3pbot6.cloudfront.net/upload/live/artists/500/3/363.jpg"
+      },
+      {
+          "artist_id": 184,
+          "artist": "Aerosmith",
+          "artwork_small": "https://d2x3f3hu3pbot6.cloudfront.net/upload/live/artists/150/6/636.jpg",
+          "artwork_large": "https://d2x3f3hu3pbot6.cloudfront.net/upload/live/artists/500/6/636.jpg"
+      },
+      {
+          "artist_id": 133,
+          "artist": "David Bowie",
+          "artwork_small": "https://d2x3f3hu3pbot6.cloudfront.net/upload/live/artists/150/8/508.jpg",
+          "artwork_large": "https://d2x3f3hu3pbot6.cloudfront.net/upload/live/artists/500/8/508.jpg"
+      },
+      {
+          "artist_id": 199,
+          "artist": "Van Halen",
+          "artwork_small": "https://d2x3f3hu3pbot6.cloudfront.net/upload/live/artists/150/6/716.jpg",
+          "artwork_large": "https://d2x3f3hu3pbot6.cloudfront.net/upload/live/artists/500/6/716.jpg"
+      },
+      {
+          "artist_id": 362,
+          "artist": "Journey",
+          "artwork_small": "https://d2x3f3hu3pbot6.cloudfront.net/upload/live/artists/150/0/1330.jpg",
+          "artwork_large": "https://d2x3f3hu3pbot6.cloudfront.net/upload/live/artists/500/0/1330.jpg"
+      }
+  ]
+};
---
+++ client/mock-top-artists.js
@@ -0,0 +1,51 @@
+export default {
+  "response": [
+      {
+          "artist_id": 3,
+          "artist": "OutKast",
+          "artwork_small": "https://d2x3f3hu3pbot6.cloudfront.net/upload/live/artists/150/4/74.jpg",
+          "artwork_large": "https://d2x3f3hu3pbot6.cloudfront.net/upload/live/artists/500/4/74.jpg"
+      },
+      {
+          "artist_id": 2,
+          "artist": "Radiohead",
+          "artwork_small": "https://d2x3f3hu3pbot6.cloudfront.net/upload/live/artists/150/6/177986.jpg",
+          "artwork_large": "https://d2x3f3hu3pbot6.cloudfront.net/upload/live/artists/500/6/177986.jpg"
+      },
+      {
+          "artist_id": 29,
+          "artist": "Kanye West",
+          "artwork_small": "https://d2x3f3hu3pbot6.cloudfront.net/upload/live/artists/150/8/177988.jpg",
+          "artwork_large": "https://d2x3f3hu3pbot6.cloudfront.net/upload/live/artists/500/8/177988.jpg"
+      },
+      {
+          "artist_id": 5523,
+          "artist": "Kendrick Lamar",
+          "artwork_small": "https://d2x3f3hu3pbot6.cloudfront.net/upload/live/artists/150/1/32361.jpg",
+          "artwork_large": "https://d2x3f3hu3pbot6.cloudfront.net/upload/live/artists/500/1/32361.jpg"
+      },
+      {
+          "artist_id": 3743,
+          "artist": "Frank Ocean",
+          "artwork_small": "https://d2x3f3hu3pbot6.cloudfront.net/upload/live/artists/150/7/12557.jpg",
+          "artwork_large": "https://d2x3f3hu3pbot6.cloudfront.net/upload/live/artists/500/7/12557.jpg"
+      },
+      {
+          "artist_id": 496,
+          "artist": "Janet Jackson",
+          "artwork_small": "https://d2x3f3hu3pbot6.cloudfront.net/upload/live/artists/150/2/16842.jpg",
+          "artwork_large": "https://d2x3f3hu3pbot6.cloudfront.net/upload/live/artists/500/2/16842.jpg"
+      },
+      {
+          "artist_id": 27,
+          "artist": "Jay-Z",
+          "artwork_small": "https://d2x3f3hu3pbot6.cloudfront.net/upload/live/artists/150/7/127.jpg",
+          "artwork_large": "https://d2x3f3hu3pbot6.cloudfront.net/upload/live/artists/500/7/127.jpg"
+      },
+      {
+          "artist_id": 31,
+          "artist": "Arcade Fire",
+          "artwork_small": "https://d2x3f3hu3pbot6.cloudfront.net/upload/live/artists/150/1/177781.jpg",
+          "artwork_large": "https://d2x3f3hu3pbot6.cloudfront.net/upload/live/artists/500/1/177781.jpg"
+      }
+  ]};
